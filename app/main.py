b53d15ea558bf40033c5c0fbf14d9c1c5e6f0849
Baran Arig <arigbaran@gmail.com>
2024-10-21 21:05:33 -0400

    numbers

--- app/main.py
+++ app/main.py
@@ -147,7 +147,7 @@ def main():
                     #i-=2
                     print(f'NUMBER {int(num)} {int(num)}.0')
                 else:
-                    print(f'NUMBER {num} {num:.{decimalPlaces-1}f}')
+                    print(f'NUMBER {num:.{decimalPlaces-1}f} {num}')
 
                 
                 
